45afcbcb2398ed45526563f062f26a9872d03148
LeAn0305 <letanbinhan150@gmail.com>
2026-05-11 11:16:20 +0700

    Lê An Đã Làm Bài Lab_01 , bao gồm : Ex01 , Ex02 ,Ex03

---
+++ lab_01/ex02/ex02_01.py
@@ -0,0 +1,5 @@
+# Nhập Tên Và Tuổi Từ Người Dùng
+ten = input("Nhập Tên Của Bạn : ")
+tuoi = input("Nhập Tuổi Của Bạn : ")
+# In Ra Thông Tin Người Dùng
+print("Chào mừng," ,ten , "! Bạn" , tuoi , "tuổi.")
